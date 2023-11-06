90e2b0233713164b1985d780f39446bddf0b9f33
Shubhang Narain <snvishere@gmail.com>
2023-11-06 15:56:32 +0530

    Layout done, with minor changes

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity,Image,FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
+
+const Welcome = () => {
+
+  const router = useRouter()
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Shubhang</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+    
+      <View style-={styles.searchContainer}ß>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+          />
+        </View>
+      </View>
+    
+    </View>
+  )
+}
+
+export default Welcome
